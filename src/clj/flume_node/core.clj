1524b1f2bccd0364304ef6a1f3e8feb0102a0457
kawasima <kawasima1016@gmail.com>
2014-01-22 20:10:36 +0900

    Add defsink.

---
+++ src/clj/flume_node/core.clj
@@ -0,0 +1,91 @@
+(ns flume-node.core
+  (:require [clojure.string :as string]
+            [clojure.java.io :as io])
+  (:import [org.apache.flume.conf FlumeConfiguration]
+           [org.apache.flume.lifecycle LifecycleSupervisor LifecycleState
+            LifecycleSupervisor$SupervisorPolicy$AlwaysRestartPolicy]
+           [org.apache.flume.node Application]))
+
+(def ^:dynamic *config*)
+(defmacro source [n & components]
+  `[:sources ~n (hash-map ~@components)])
+
+(defmacro sink [n & components]
+  `[:sinks ~n (hash-map ~@components)])
+
+(defmacro channel [n & components]
+  `[:channels ~n (hash-map ~@components)])
+
+
+(defmacro defagent [agt & body]
+  `(alter-var-root (var *config*)
+                   (constantly
+                    {~agt
+                     (reduce (fn [ks# [c# n# attrs#]]
+                               (assoc-in ks# [c# n#] attrs#))
+                             {} [~@body])})))
+
+(defmacro defsink [sink-name & body]
+  `(def ~sink-name (hash-map ~@body)))
+
+(defn convert-config-map!
+  ([config-map form ks]
+   (cond
+    (map? form) (doall (map #(convert-config-map! config-map (second %) (conj ks (first %))) form))
+    :else (.put config-map
+                (clojure.string/join "." (map name ks))
+                (cond (keyword? form) (name form)
+                      :else (str form)))))
+  ([config-map form] (convert-config-map! config-map form [])))
+
+(defn component-names [config-map component-type]
+  (let [agent-key (first (keys config-map))]
+    (->> (get-in config-map [agent-key component-type])
+         keys
+         (map name)
+         string/join)))
+
+(defn clj-map-configuration-provider [agent-name config-map]
+  (proxy [org.apache.flume.node.AbstractConfigurationProvider] [agent-name]
+    (getFlumeConfiguration
+     []
+     (let [config-java (java.util.HashMap.)]
+       (convert-config-map! config-java config-map)
+       (doto config-java
+         (.put (str agent-name ".sources")
+               (component-names config-map :sources))
+
+         (.put (str agent-name ".sinks")
+               (component-names config-map :sinks))
+
+         (.put (str agent-name ".channels")
+               (component-names config-map :channels)))
+       (FlumeConfiguration. config-java)))))
+
+
+(defn make-app
+  ([] (make-app *config*))
+  ([config]
+   (let [agent-name (name (first (keys config)))
+         supervisor (LifecycleSupervisor.)
+         configuration-provider (clj-map-configuration-provider agent-name config)
+         configuration (.getConfiguration configuration-provider)
+         application (Application.)]
+     (. application handleConfigurationEvent configuration)
+     (println (-> configuration-provider
+                  .getFlumeConfiguration
+                  (.getConfigurationFor agent-name)))
+     (. application start)
+     application)))
+
+
+
+(defn -main []
+  (binding [*ns* (find-ns 'flume-node.core)]
+    (-> "flume_conf.clj"
+      io/resource
+      io/as-file
+      .getPath
+      load-file))
+  (let [application (make-app)]))
+
---
+++ src/jvm/net/unit8/flume_node/ClojureSink.java
@@ -0,0 +1,96 @@
+package net.unit8.flume_node;
+
+import org.apache.flume.Channel;
+import org.apache.flume.Context;
+import org.apache.flume.CounterGroup;
+import org.apache.flume.Event;
+import org.apache.flume.EventDeliveryException;
+import org.apache.flume.Sink;
+import org.apache.flume.sink.AbstractSink;
+import org.apache.flume.Transaction;
+import org.apache.flume.conf.Configurable;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import clojure.lang.RT;
+import clojure.lang.Keyword;
+import clojure.lang.Var;
+import clojure.lang.IFn;
+
+import com.google.common.base.Preconditions;
+
+/**
+ * This sink can execute a custom process written by clojure.
+ */
+public class ClojureSink extends AbstractSink implements Configurable {
+  private static final Logger logger = LoggerFactory.getLogger(ClojureSink.class);
+  private String namespace;
+  private String sinkName;
+
+  private CounterGroup counterGroup;
+  private Var configureFunc;
+  private Var startFunc;
+  private Var stopFunc;
+  private Var processFunc;
+
+  public ClojureSink() {
+    counterGroup = new CounterGroup();
+  }
+
+  @Override
+  public void configure(Context context) {
+    String nsname = context.getString("nsname");
+    Preconditions.checkState(nsname != null, "No nsname specified");
+    String[] tokens = nsname.split("/", 2);
+    Preconditions.checkState(tokens.length == 2, "nsname must be [namespace]/[sink-name]");
+
+    Var sinkVar = RT.var(tokens[0], tokens[1]);
+    Preconditions.checkState(sinkVar != null, tokens[0] + "/" + tokens[1] + " is not defined");
+    Object sink = sinkVar.get();
+
+    Var getFunc = RT.var("clojure.core", "get");
+    IFn startFunc   = (IFn) getFunc.invoke(sink, Keyword.intern("start"));
+    IFn stopFunc    = (IFn) getFunc.invoke(sink, Keyword.intern("stop"));
+    IFn processFunc = (IFn) getFunc.invoke(sink, Keyword.intern("process"));
+    Preconditions.checkState(processFunc != null, "No :process function");
+  }
+
+  @Override
+  public void start() {
+    if (startFunc != null)
+      startFunc.invoke();
+  }
+
+  @Override
+  public void stop() {
+    if (stopFunc != null)
+      stopFunc.invoke();
+  }
+
+  @Override
+  public Status process() throws EventDeliveryException {
+    Status result = Status.READY;
+    Channel channel = getChannel();
+    Transaction transaction = channel.getTransaction();
+    Event event = null;
+
+    try {
+      transaction.begin();
+      event = channel.take();
+
+      if (event != null) {
+        processFunc.invoke(event);
+      } else {
+        result = Status.BACKOFF;
+      }
+      transaction.commit();
+    } catch (Exception ex) {
+      transaction.rollback();
+      logger.error("Failed to deliver event. Exception follows.", ex);
+      throw new EventDeliveryException("Failed to deliver event: " + event, ex);
+    } finally {
+      transaction.close();
+    }
+    return result;
+  }
+}
