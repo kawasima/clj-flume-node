2db0f524218a0d25ac64bbc832ea1717e84e76ea
kawasima <kawasima1016@gmail.com>
2014-01-24 15:14:01 +0900

    Added license.

--- project.clj
+++ project.clj
@@ -1,4 +1,10 @@
 (defproject net.unit8/clj-flume-node "0.1.0"
+  :description "A clojure library for Apache Flume."
+  :url "https://github.com/kawasima/clj-flume-node.git"
+  :license {:name "Eclipse Public License - v 1.0"
+            :url "http://www.eclipse.org/legal/epl-v10.html"
+            :distribution :repo
+            :comments "same as Clojure"}
   :dependencies [[org.clojure/clojure "1.5.1"]
                  [org.apache.flume/flume-ng-node "1.4.0"]]
   :source-paths ["src/clj"]
